8c3a256c4d0f39ddba3558cba3980bb9e6c0f8b8
yazoo1220 <podman1220@hotmail.com>
2023-06-18 01:39:32 +0300

    Update app.py

--- app.py
+++ app.py
@@ -79,6 +79,7 @@ if chat_button:
         chat_history = []
         chain = load_chain()
         result = chain.predict(input=user_input)
+        memory.save_context({"input": user_input}, {"output": result})
         st.session_state.past.append(user_input)
         st.session_state.generated.append(result)
 
